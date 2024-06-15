9b81f13e87147d463e4ce06766ebfa04d144695b
MMNLakna <136817118+MMNLakna@users.noreply.github.com>
2024-06-15 12:08:30 +0530

    minor changes

--- src/components/sections/ChooseUs.tsx
+++ src/components/sections/ChooseUs.tsx
@@ -1,32 +1,16 @@
 import Accordian from './Accordian';
 
 const ChooseUs = () => {
-    const links = [
-        {
-            title: "TRUSTED CLIENTS"
-        },
-        {
-            title: "lasdasdasdasdasdasdasfknaslknaslkdnas"
-        }, {
-            title: "lasdasdasdasdasdasd"
-        }, {
-            title: "lasdasdasdasdasdasd"
-        }, {
-            title: "lasdasdasdasdasdasd"
-        }, {
-            title: "lasdasdasdasdasdasd"
-        },
-    ]
     return (
-        <div className="flex justify-around gap-10 my-20 max-md:flex-col max-md:justify-center max-md:items-center   px-3">
+        <div className="flex justify-around gap-10 mt-20 max-md:flex-col max-md:justify-center max-md:items-center px-3">
             <div>
                 <div className="my-6">
                     <div className="text-2xl font-semibold">
                         TRUSTED CLIENTS
                     </div>
-                    <div>TRUSTED BY CLIENTS WORLDWIDE. SHIPPING EXCELLENCE.</div>
+                    <div className='tracking-wider'>TRUSTED BY CLIENTS WORLDWIDE. SHIPPING EXCELLENCE.</div>
                 </div>
-                <img src="../../../public/images/Group 6.png" width={500} alt="" />
+                <img src="../../../public/images/Group 7.png" width={500} alt="" />
 
             </div>
             <div className=' w-1/2 max-md:w-full'>
@@ -34,7 +18,7 @@ const ChooseUs = () => {
                     <div className="text-2xl font-semibold">
                         WHY CHOOSE US
                     </div>
-                    <div>RELIABLE, EFFICIENT, TAILORED SHIPPING SOLUTIONS.</div>
+                    <div className='tracking-wider'>RELIABLE, EFFICIENT, TAILORED SHIPPING SOLUTIONS.</div>
                 </div>
 
                 <div className="">
